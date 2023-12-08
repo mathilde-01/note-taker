6add52c3e7305f15a34065c184c0bd51fc7c8c73
mathilde-01 <mathilde.7892@gmail.com>
2023-12-08 16:09:43 -0500

    adding delete function

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 // express import
 const express = require("express");
+
+// database path
+const dbJson = require("./db/db.json");
+
 // path
 const path = require("path");
 
@@ -31,6 +35,7 @@ app.get("/api/notes", (req, res) => {
   });
 });
 
+// post read and write methods
 app.post("/api/notes", (req, res) => {
   console.log(req.body);
 
@@ -56,11 +61,32 @@ app.post("/api/notes", (req, res) => {
   }
 });
 
+// Delete note
+app.delete("/api/notes/:id", (req, res) => {
+    let deletedNote = parseInt(req.params.id);
+    console.log(deletedNote);
+
+    for (let i = 0; i < dbJson.length; i++) {
+        if (deletedNote === dbJson[i].id) {
+            dbJson.splice(i, 1);
+
+            let noteJson = JSON.stringify(dbJson, null, 2);
+            console.log(noteJson);
+            fs.writeFile("./db/db.json", noteJson, function (err) {
+                if (err) throw err;
+                console.log("Your note has been deleted!");
+                res.json(dbJson);
+            });
+        }
+    }
+});
+
 // GET Route for homepage
 app.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/notes.html"))
 );
 
+// if no matching route is found, default to home
 app.get("*", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/index.html"))
 );
